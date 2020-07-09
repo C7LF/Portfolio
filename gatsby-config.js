4137c0a822f7288c51507a3cf11af1ba32834a0e
C7LF <ccal72@yahoo.co.uk>
2020-07-09 17:26:44 +0100

    added fontawesome css

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-material-ui`
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-fontawesome-css`
   ],
 }
